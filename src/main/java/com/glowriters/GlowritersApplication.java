b7f5a2ac0b7e4b15abaf480abbc6b5c9b2ec6bf4
cldhfleks2 <cldhfleks2@gmail.com>
2024-05-28 23:31:38 +0900

    server setting done.

---
+++ src/main/java/com/glowriters/GlowritersApplication.java
@@ -0,0 +1,13 @@
+package com.glowriters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlowritersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlowritersApplication.class, args);
+	}
+
+}
