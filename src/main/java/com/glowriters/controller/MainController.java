ebaeb8e488f08b2586237ba743b7cadc99de778b
cldhfleks2 <cldhfleks2@gmail.com>
2024-06-05 01:29:10 +0900

    240605 main controller 수정2

--- src/main/java/com/glowriters/controller/MainController.java
+++ src/main/java/com/glowriters/controller/MainController.java
@@ -1,20 +1,13 @@
 package com.glowriters.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import com.glowriters.domain.MainSlideDTO;
-import com.glowriters.domain.Member;
 import com.glowriters.service.KakaoService;
 
 import jakarta.servlet.http.HttpSession;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -31,11 +24,11 @@ public class MainController extends BaseController {
 		model.addAttribute("kakaoUrl", kakaoService.getKakaoLogin());
 		
 		//메인 슬라이드(전체 게시물)
-		List<MainSlideDTO> mainSlideItems = new ArrayList<>();
+//		List<MainSlideDTO> mainSlideItems = new ArrayList<>();
 		
 		
 		
-		model.addAttribute("mainSlideItems", mainSlideItems);
+//		model.addAttribute("mainSlideItems", mainSlideItems);
 		
 		
 		
